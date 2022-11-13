6bb99a3f51903bf438a8fb3094be18ddba7dc5a5
Jakub <jakubmskowron@gmail.com>
2022-11-13 22:12:19 +0100

    Database creation

--- app.py
+++ app.py
@@ -1,48 +1,49 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+#app & db
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "secret_key"
 
+db = SQLAlchemy(app)
 
-hops = [{'hop':'Lubelski', 'feature':'Bitter'},
-        {'hop':'Cascade', 'feature':'Aroma'},
-        {'hop':'Citra', 'feature':'Aroma'},
-        {'hop':'Mosaic', 'feature':'Aroma'},
-        {'hop':'Amarillo', 'feature':'Aroma'},
-        {'hop':'Columbus', 'feature':'Bitter'}
-        ]
+#models
+class Hop(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    feature = db.Column(db.String(50))
+    description = db.Column(db.String(120))
+
+    def __repr__(self) -> str:
+        return f"{self.name} - {self.feature} - {self.description}"
+
 
 @app.route('/hops_list', methods=["GET"])
 def get_hops_list():
-    result = [x['hop'] for x in hops]
-    return jsonify({"Hops list": result})
+    hops = Hop.query.all()
+    output = []
+    for hop in hops:
+        hop_data = {
+            "id":hop.id, 
+            "name":hop.name,
+            "feature":hop.feature, 
+            "description":hop.description
+            }
+        output.append(hop_data)
+    return {"hops list": output}
 
 @app.route('/hops_list/<int:hop_id>', methods=["GET"])
 def get_hops_specification(hop_id: int):
-    return jsonify({"Hop specification": hops[hop_id]})
+    hop = Hop.query.get_or_404(hop_id)
+    return jsonify({
+            "id":hop.id, 
+            "name":hop.name,
+            "feature":hop.feature, 
+            "description":hop.description
+            })
     
-
-@app.route('/hops_list', methods=["POST"])
-def add_new_hop():
-    hop = {
-        'hop': request.json['hop'],
-        'feature': request.json['feature'],
-        }
-    hops.append(hop)
-    return jsonify({"Hops list": hops})
-
-@app.route('/hops_list/<int:hop_id>', methods=["DELETE"])
-def delete_hop(hop_id: int):
-    hops.pop(hop_id)
-    return jsonify({"Hops list": hops})
-
-@app.route('/hops_list/<int:hop_id>', methods=["PUT"])
-def update_hop(hop_id: int):
-    hop = {
-        'hop': request.json['hop'],
-        'feature': request.json['feature'],
-        }
-    hops[hop_id] = hop
-    return jsonify({"Hops list": hops})
-
 if __name__ == "__main__":
     app.run(debug = True)
