112f51a519381e1daf93a057dfbfc566c3a4309f
Jakub <jakubmskowron@gmail.com>
2022-11-22 00:22:05 +0100

    GET methods modification

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
+'''
+API helps finding hops substitutes
+'''
 #app & db
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mongodb://admin:ifPqBVdO3sGwundRxz593OJY@MongoS3601A.back4app.com:27017/91871e87fb9a4be891a477b0a57d57aa"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "secret_key"
 
@@ -14,11 +17,10 @@ db = SQLAlchemy(app)
 class Hop(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
-    feature = db.Column(db.String(50))
-    description = db.Column(db.String(120))
+    substitutes = db.Column(db.String(120))
 
     def __repr__(self) -> str:
-        return f"{self.name} - {self.feature} - {self.description}"
+        return f"{self.name} - {self.substitutes}"
 
 
 @app.route('/hops_list', methods=["GET"])
@@ -29,11 +31,11 @@ def get_hops_list():
         hop_data = {
             "id":hop.id, 
             "name":hop.name,
-            "feature":hop.feature, 
-            "description":hop.description
+            "substitutes":hop.substitutes
             }
         output.append(hop_data)
     return {"hops list": output}
+
 
 @app.route('/hops_list/<int:hop_id>', methods=["GET"])
 def get_hops_specification(hop_id: int):
@@ -41,9 +43,9 @@ def get_hops_specification(hop_id: int):
     return jsonify({
             "id":hop.id, 
             "name":hop.name,
-            "feature":hop.feature, 
-            "description":hop.description
+            "substitutes":hop.substitutes
             })
-    
+
+
 if __name__ == "__main__":
     app.run(debug = True)
