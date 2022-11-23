d0a7851e65468c0bc7ffbdbcacd98b466bd2504a
Jakub <jakubmskowron@gmail.com>
2022-11-23 12:03:52 +0100

    DB added

--- app.py
+++ app.py
@@ -7,20 +7,27 @@ API helps finding hops substitutes
 #app & db
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mongodb://admin:ifPqBVdO3sGwundRxz593OJY@MongoS3601A.back4app.com:27017/91871e87fb9a4be891a477b0a57d57aa"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = "secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret_key'
 
 db = SQLAlchemy(app)
 
 #models
 class Hop(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True)
-    substitutes = db.Column(db.String(120))
+    name = db.Column(db.String(32))
+    alpha = db.Column(db.String(32))
+    beta = db.Column(db.String(32))
+    origin = db.Column(db.String(32))
+    description = db.Column(db.String(120))
+    aroma = db.Column(db.String(120))
+    typical_beer_styles = db.Column(db.String(120))
+    used_for = db.Column(db.String(120))
+    substitutions = db.Column(db.String(120))
 
     def __repr__(self) -> str:
-        return f"{self.name} - {self.substitutes}"
+        return self.name
 
 
 @app.route('/hops_list', methods=["GET"])
@@ -28,24 +35,14 @@ def get_hops_list():
     hops = Hop.query.all()
     output = []
     for hop in hops:
-        hop_data = {
-            "id":hop.id, 
-            "name":hop.name,
-            "substitutes":hop.substitutes
-            }
-        output.append(hop_data)
+        output.append(hop)
     return {"hops list": output}
-
-
-@app.route('/hops_list/<int:hop_id>', methods=["GET"])
-def get_hops_specification(hop_id: int):
-    hop = Hop.query.get_or_404(hop_id)
-    return jsonify({
-            "id":hop.id, 
-            "name":hop.name,
-            "substitutes":hop.substitutes
-            })
-
+'''
+@app.route('/hops_list/<int:id_hop>', methods=['GET'])
+def get_hop_description(id_hop):
+    result = Hop.query.
+    return hops[id_hop-1]
+'''
 
 if __name__ == "__main__":
     app.run(debug = True)
