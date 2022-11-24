4d37d538c6bf21604e65dfd3036dcd51c4fa7767
Jakub <jakubmskowron@gmail.com>
2022-11-24 23:00:57 +0100

    Added new query strings, updated requirements.txt (request module added)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from json import JSONEncoder
+import random
 
 '''
 API helps finding hops substitutes
@@ -15,11 +16,6 @@ app.config['SECRET_KEY'] = 'secret_key'
 db = SQLAlchemy(app)
 
 #models
-
-class MyEncoder(JSONEncoder):
-    def default(self, obj):
-        return obj.__dict__    
-
 class Hop(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), unique=True)
@@ -46,23 +42,57 @@ class Hop(db.Model):
             "substitutions": self.substitutions
         }
         return hop_dict
+
 #routes
 
-#all hops
+#all hops, or hops filtering by: name, useing for and typical beer styles 
 
-@app.route('/hops_list', methods=["GET"])
-def get_hops_list():
-    hops = Hop.query.all()
-    output = []
-    for hop in hops:
-        output.append(hop.as_dict())
-    return jsonify({"hops list": output})
+@app.route('/hops_list/', methods=["GET"])
+def get_hops():
+    name = request.args.get('name', None)
+    used_for = request.args.get('used_for', default=None)
+    beer_style = request.args.get('beer_style', default=None)
+    alpha_gt = request.args.get('alpha_gt', default=None)
+    alpha_lt = request.args.get('alpha_lt', default=None)
+    beta_gt = request.args.get('beta_gt', default=None)
+    beta_lt = request.args.get('beta_lt', default=None)
+    result = []
+    if name is not None:
+        hops = Hop.query.filter_by(name = name).first()
+        result.append(hop.as_dict())
+    if used_for is not None:
+        hops = Hop.query.filter_by(used_for = used_for).all()
+        for hop in hops:
+            result.append(hop.as_dict())
+    if beer_style is not None:
+        beer_style = beer_style.lower()
+        hops = Hop.query.all()
+        for hop in hops:
+            if beer_style in (hop.typical_beer_styles).lower():
+                result.append(hop.as_dict())
+    if len(request.args) == 0:
+        hops = Hop.query.all()
+        for hop in hops:
+            result.append(hop.as_dict())
+    return jsonify(result)
 
 #hops filter by id
 
 @app.route('/hops_list/<int:id>', methods=['GET'])
-def get_hop_description(id):
-    hop = Hop.query.get_or_404(id)
+def get_hops_description(id):
+    hop = Hop.query.get(id)
+    return jsonify(hop.as_dict())
+
+#random hops
+
+@app.route('/hops_list/random', methods=['GET'])
+def get_random_hops_description():
+    hops = Hop.query.all()
+    output = []
+    for hop in hops:
+        output.append(hop.id)
+    id = random.choice(output)
+    hop = Hop.query.get(id)
     return jsonify(hop.as_dict())
 
 #new hops
@@ -82,13 +112,13 @@ def add_new_hop():
                 )
     db.session.add(new_hop)
     db.session.commit()
-    return jsonify(new_hop)
+    return jsonify(new_hop.as_dict())
 
 #delete hops
 
 @app.route('/hops_list/<int:id>', methods=["DELETE"])
 def del_new_hop(id):
-    hop_to_del = Hop.query.get_or_404(id)
+    hop_to_del = Hop.query.get(id)
     db.session.delete(hop_to_del)
     db.session.commit()
     return jsonify({"deleted": "successfully"})
