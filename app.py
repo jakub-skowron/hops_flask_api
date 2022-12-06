8c9bb0ea5554289692d39af1987dc52494a9200d
Jakub <jakubmskowron@gmail.com>
2022-12-07 00:03:48 +0100

    DB change

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 '''
 API helps finding hops substitutes
 '''
-#app & db
 
 app = Flask(__name__)
 '''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
 '''
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:UcoRUj5Zuw36xThWZjT4@containers-us-west-154.railway.app:6878/railway"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:luLBk2hENxANvCtoXFgM@containers-us-west-159.railway.app:6143/railway"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret_key'
 
