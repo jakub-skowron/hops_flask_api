4fa3983088f0405f9d02a53ea09d2b907aa89778
Jakub <jakubmskowron@gmail.com>
2022-12-06 22:58:42 +0100

    Code update

--- app.py
+++ app.py
@@ -7,13 +7,17 @@ API helps finding hops substitutes
 #app & db
 
 app = Flask(__name__)
+'''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+'''
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:UcoRUj5Zuw36xThWZjT4@containers-us-west-154.railway.app:6878/railway"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret_key'
 
 db = SQLAlchemy(app)
 
 import routes
+db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run()
