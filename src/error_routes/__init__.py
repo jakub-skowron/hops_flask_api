5c71c984327ecc0147b89c474af780f79864338f
Jakub Skowron <jakubmskowron@gmail.com>
2023-03-02 12:02:01 +0100

    Update of docekr-compose added

--- src/error_routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint("error_routes", __name__)
-
-from src.error_routes import error_routes
--- src/hops/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint("hops", __name__, url_prefix="/api/v1/hops")
-
-from src.hops import hops
